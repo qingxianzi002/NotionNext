40b4e856e691fb455eb142a9e28db9f337c52232
清弦子 <113122041+qingxianzi002@users.noreply.github.com>
2026-04-19 17:35:41 +0800

    Update BlogPostListPage.js

--- themes/hexo/components/BlogPostListPage.js
+++ themes/hexo/components/BlogPostListPage.js
@@ -29,7 +29,6 @@ const BlogPostListPage = ({ page = 1, posts = [], postCount, siteInfo }) => {
               index={posts.indexOf(post)}
               key={post.id}
               post={post}
-              prefetch={index < 5} 
               siteInfo={siteInfo}
             />
           ))}
